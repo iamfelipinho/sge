fd75b803953a3548f42ae4d14137d544a3f40a74
iamfelipinho <123705772+iamfelipinho@users.noreply.github.com>
2024-03-19 21:05:45 -0300

    seila

---
+++ frontend/sge-front/pages/home/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const{ signOut } = useAuth();
+    definePageMeta({
+        middleware: 'auth'
+    })
+
+    const submitLogout = ()=>{
+        signOut({redirect: false}).then(()=>{
+            console.log("Successfully performed the logout");
+            navigateTo("/")
+        }).catch((error)=>{
+            console.log("error when trying logout ", error);
+        })
+
+    }
+</script>
+
+<template>
+    <div>
+        <h1>minha tela home</h1>
+        <button @click="submitLogout" class="customButton">FAZER LOGOUT</button>
+    </div>
+</template>
+
+<style lang="scss">
+</style>
